edaa59cc1dc77a74e3109f178d56101f29e2189a
mehul79 <mehulrgupta@gmail.com>
2025-08-09 05:40:05 +0530

    upt turborepo

--- turborepo/apps/be/prisma/migrations/20250319200845_profile_pic_added/migration.sql
+++
@@ -1,2 +0,0 @@
--- AlterTable
-ALTER TABLE "User" ADD COLUMN     "profilePic" TEXT NOT NULL DEFAULT 'https://res.cloudinary.com/dtmrwsnag/image/upload/v1742414335/avatar_03-removebg-preview_1_ehvo4y.png';
